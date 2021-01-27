12c1b9f031af5db672860d11d3cadf63b512b809
Aung Myat Moe <aungmyatmoe.edu@gmail.com>
2021-01-27 19:12:57 +0630

    Syntax

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Copyright by "Aung Myat Moe"
-# get file name
+
+# get input file name
     FILE=$1
 
 # split with `.` delimeter
@@ -9,12 +10,20 @@
 # check file name exist or not
 if [[ $# -eq 0 ]]; then
     echo "File Name Required!"
+
     
 elif [[ ${NAME[1]} != "cpp" ]]; then
     echo "File extension should be 'cpp'"
 else
+
+if [[ ! -d "target" ]]; then
+    mkdir target
+fi
+
 # compile file and put into `target` directory
     $(g++ $FILE -o target/${NAME[0]})
+    
 # run output binary file from traget directory
     ./target/${NAME[0]}
+
 fi

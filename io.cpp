efc03b04688728fc0a76d6aed5ab4fe28552dfcd
Aung Myat Moe <aungmyatmoe.edu@gmail.com>
2021-01-27 19:15:52 +0630

    io

---
+++ io.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+    std::cout << "Hello ";
+    std::cout << "World" << std::endl;
+    
+    // new line
+    std::cout << "Welcome! \n Aung Myat Moe";
+
+}

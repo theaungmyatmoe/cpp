553dcd962ce581a5e4c43c36ebee91fa527b8045
Aung Myat Moe <aungmyatmoe.edu@gmail.com>
2021-01-27 20:21:56 +0630

    str length and size

--- strings.cpp
+++ strings.cpp
@@ -2,7 +2,27 @@
 #include<string>
 using namespace std;
 int main(){
+    
     string name = "Aung Myat Moe";
     cout << name << std::endl;
+    
+    
+    // string concatination
+    
+    string fname = "Aung ";
+    string midname = "Myat ";
+    string lname =  "Moe";
+    string fullName = fname + midname;
+    
+    // append() is faster than `+`
+    
+    fullName.append(lname);
+    std::cout << fullName << std::endl;
+    
+    // string length() or size()
+    
+    string alphas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    cout<< "Length of alphas => " << alphas.length();
+    
     
 }

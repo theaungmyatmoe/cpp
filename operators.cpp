3faf36fea79d4e21e57a0965b3fb2c793a7c1895
Aung Myat Moe <aungmyatmoe.edu@gmail.com>
2021-01-27 19:31:51 +0630

    operators

---
+++ operators.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int main(){
+    int a = 100;int b = 5;
+    int sum = a  + b;
+    int subtraction = a - b;
+    int multiplication = a * b;
+    int division = a/b;
+    int modolus = a%b;
+    
+    std::cout << "Sum is => " << sum << std::endl;
+    std::cout << "Subtraction is => " << subtraction << std::endl;
+    std::cout << "Multiplication is => " << multiplication << std::endl;
+    std::cout << "Division is => " << division << std::endl;
+    std::cout << "Modolus is => " << modolus << std::endl;
+}

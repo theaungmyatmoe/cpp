10f074e2d6f4ba3084da501e35f7fafdef5fd4b9
Aung Myat Moe <aungmyatmoe.edu@gmail.com>
2021-01-27 19:27:10 +0630

    user input

---
+++ input.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+int main() {
+
+    int x;
+    std::cout << "Input A Number" << std::endl;
+    std::cin >> x;
+    std::cout << "Your Number is: " << x << std::endl;
+
+}
--- vars.cpp
+++ vars.cpp
@@ -5,7 +5,7 @@ int main() {
     int num = 200;
     string name = "Aung Myat Moe";
     char catName[] = "Noosi";
-    float PI = 3.142;
+   const float PI = 3.142;
     double age = 17.0;
     bool isSingle = true;
     cout << num << endl;

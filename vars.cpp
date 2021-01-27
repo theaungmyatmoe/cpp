53ea0859f49933a8598f7683f92203b32a384767
Aung Myat Moe <aungmyatmoe.edu@gmail.com>
2021-01-27 19:21:32 +0630

    variables

---
+++ vars.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+    int num = 200;
+    string name = "Aung Myat Moe";
+    char catName[] = "Noosi";
+    float PI = 3.142;
+    double age = 17.0;
+    bool isSingle = true;
+    cout << num << endl;
+    cout << name << endl;
+    cout << catName << endl;
+    cout << PI << endl;
+    cout << age << endl;
+    cout << isSingle << endl;
+    
+}

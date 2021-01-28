c7bc880a509f57ab777471fdf74c1d54aabb0c2e
Aung Myat Moe <aungmyatmoe.edu@gmail.com>
2021-01-28 21:29:46 +0630

    Function With Paras

--- function.cpp
+++ function.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
-void getName(); // function declaration
+#include<string>
+using namespace std;
+// declaration
+void getName();
+void getStudent(string name);
 
 int main() {
     getName();
+    getStudent("Noosi");
 }
 
 void getName() {
-    std::cout << "Aung Myat Moe" << std::endl;
+    cout << "Aung Myat Moe" << endl;
+}
+
+void getStudent(string name){
+    cout << name << endl;
 }
